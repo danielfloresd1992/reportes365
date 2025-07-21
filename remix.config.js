8e645f822aec653860deb0b713c8c4b17df8a45c
Daniel 'Oficina <danielfloresd92@gmail.com>
2025-07-21 14:44:59 -0400

    build

--- remix.config.js
+++ remix.config.js
@@ -1,16 +1,8 @@
 import { netlifyEdgeAdapter } from "@netlify/remix-edge-adapter";
-import { vitePlugin as remix } from "@remix-run/dev";
 
-/** @type {import('@remix-run/dev').AppConfig} */
 export default {
     ignoredRouteFiles: ["**/.*"],
-    server: netlifyEdgeAdapter(),  // Para Edge Functions
-    // server: netlifyAdapter(),   // Para funciones estándar (Node.js)
+    server: netlifyEdgeAdapter(),
     serverBuildPath: ".netlify/edge-functions/server.js",
-    tailwind: true,
-    postcss: true,
-    watchPaths: ["./tailwind.config.ts"],
-    future: {
-        v3_fetcherPersist: true,
-    },
+    // Mantén el resto de tu configuración
 };
