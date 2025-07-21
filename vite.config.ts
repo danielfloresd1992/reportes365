0eff650ddac4784fba0ca06175b6a1aff432a7d0
Daniel 'Oficina <danielfloresd92@gmail.com>
2025-07-21 13:02:20 -0400

    Adaptador para despliegue en remix

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,40 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 
 export default defineConfig({
 
-  server: { host: '0.0.0.0' },
+    server: { host: '0.0.0.0' },
 
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-    nodePolyfills({
-      include: ['url'],
-      protocolImports: true
-    })
-  ],
-  build: {
-    minify: 'esbuild',
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
+    plugins: [
+        remix({
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+            },
+        }),
+        tsconfigPaths(),
+        nodePolyfills({
+            include: ['url'],
+            protocolImports: true
+        }),
+        netlifyPlugin()
+    ],
+    build: {
+        minify: 'esbuild',
+        chunkSizeWarningLimit: 2000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor'
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
 });
