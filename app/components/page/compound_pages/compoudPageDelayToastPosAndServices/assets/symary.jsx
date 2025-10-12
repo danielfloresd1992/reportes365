a4fb51d54b2e21fbb51cabfd1a92931721b86957
Daniel 'Oficina <danielfloresd92@gmail.com>
2025-10-12 17:52:33 -0400

    Finalización del modulo summary en toast Pos

--- app/components/page/compound_pages/compoudPageDelayToastPosAndServices/assets/symary.jsx
+++ app/components/page/compound_pages/compoudPageDelayToastPosAndServices/assets/symary.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, memo } from 'react';
+import { useState, useEffect, useRef, memo } from 'react';
 import { useSelector } from 'react-redux';
 import LayautNovelty from '../../../../layaut/LayautPage';
 
@@ -27,11 +27,16 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
 
     const styleCellBorder = 'border border-black text-lg';
 
+
+    let totalTiketDelay = 0;
     const totalDelayToast = dataState.delayToastPost.delay.length;
     const totalDelayDelivety = dataState.delayDeliveryDishWhenItIsReadyInKitchen.delay.length;
 
+
     for (let category in dataState.delayToastPost.categoryMetrics) {
-        console.log(category);
+        if (category !== 'take out') {
+            totalTiketDelay = totalTiketDelay + dataState.delayToastPost.categoryMetrics[category].totalTickets;
+        }
     }
 
 
@@ -56,10 +61,6 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
 
 
 
-    console.log(dataState);
-    console.log(dishItem);
-
-
 
     const average = (write = false) => {
 
@@ -68,8 +69,7 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
 
             const newData = { ...dataState.delayToastPost }
             newData.categoryMetrics[nameDish].avgPreparation = value;
-            console.log(newData);
-            // put_delay(newData, dataState.delayToastPost.type);
+            put_delay(newData, dataState.delayToastPost.type);
         };
 
 
@@ -83,6 +83,7 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
                         <td className={styleCellBorder}>{dish.nameDishe} </td>
                         <td className={styleCellBorder}>
                             <p
+
                                 contentEditable={write}
                                 onBlur={(e) => put(e.target.textContent)}
                             >{delays.avgPreparation}</p>
@@ -94,15 +95,59 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
                 <div className='w-full flex justify-between'>
 
                     <div>
-                        <p className='text-gray-600 block'>{dish.nameDishe}:</p>
+                        <p className='monotext text-gray-600 block'>{dish.nameDishe}:</p>
                     </div>
                     <div>
-                        <p className='font-medium text-green-600' contentEditable={write} onBlur={(e) => put(e.target.textContent, dish.nameDishe)}> {delays.avgPreparation}</p>
+                        <p className='monotext font-medium' contentEditable={write} onBlur={(e) => put(e.target.textContent, dish.nameDishe)}> {delays.avgPreparation}</p>
                     </div>
                 </div>
             )
         });
-    }
+    };
+
+
+
+    const process = (write = false) => {
+
+        const put = (value, nameDish) => {
+            const newData = { ...dataState.delayToastPost }
+            newData.categoryMetrics[nameDish].totalTickets = Number(value);
+            put_delay(newData, dataState.delayToastPost.type);
+        };
+
+
+
+        return dishItem.map(dish => {
+            const delays = dataState.delayToastPost.categoryMetrics[dish.nameDishe];
+
+
+            if (!(dishItem.length > 2 && dishItem.length < 5)) {
+                return (
+                    <>
+                        <td className={styleCellBorder}>{dish.nameDishe} </td>
+                        <td className={styleCellBorder}>
+                            <p
+
+                                contentEditable={write}
+                                onBlur={(e) => put(e.target.textContent)}
+                            >{delays.totalTickets}</p>
+                        </td>
+                    </>
+                )
+            }
+            return (
+                <div className='w-full flex justify-between'>
+
+                    <div>
+                        <p className='monotext text-gray-600 block'>{dish.nameDishe}:</p>
+                    </div>
+                    <div>
+                        <p className='monotext font-medium min-w-[20px] text-right' contentEditable={write} onBlur={(e) => put(e.target.textContent, dish.nameDishe)}> {delays.totalTickets}</p>
+                    </div>
+                </div>
+            )
+        });
+    };
 
 
     if (!config?.propMetricTableInToastPos) return null;
@@ -117,17 +162,17 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
             {
                 dishItem.length > 2 && dishItem.length < 5 ?
 
-                    <div className="w-full h-full">
-                        <div className="w-full h-full">
+                    <div className='w-full h-full'>
+                        <div className='w-full h-full'>
                             <div className='w-full h-[50%] flex direction-row  justify-around'>
-                                <div className="w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]">
+                                <div className='w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]'>
                                     <div className='w-full flex flex-row gap-[.5rem] text-lg font-semibold text-gray-800 border-b border-gray-200 pb-[.5rem] mb-[.5rem]'>
                                         <img className='w-[25px]' src={icoGrafic} alt='ico-grafuc' />
-                                        <h2 className="w-full">Resumen General</h2>
+                                        <h2 className='w-full'>Resumen General</h2>
                                     </div>
 
 
-                                    <div className="flex justify-between py-[.1rem] border-b border-gray-100 monotext">
+                                    <div className='flex justify-between py-[.1rem] border-b border-gray-100 monotext'>
                                         <div className='w-[50%]'>
                                             <p className="w-full text-gray-600">Procesos evaluados</p>
                                         </div>
@@ -135,12 +180,12 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
                                             <p style={{
                                                 display: 'block',
                                                 textAlign: 'end'
-                                            }} className="font-medium text-gray-900 " >{/*dishItem.length > 1 ? (bodyState['take out'] ? (totalProcess().totalProcess - bodyState['take out'].totalProcess) : totalProcess().totalProcess) : 0*/}</p>
+                                            }} className='font-medium text-gray-900' >{totalTiketDelay}</p>
                                         </div>
                                     </div>
 
 
-                                    <div className="flex justify-between py-[.1rem] border-b border-gray-100 text-green-600 monotext">
+                                    <div className='flex justify-between py-[.1rem] border-b border-gray-100 text-green-600 monotext'>
                                         <div className='w-[50%]'>
                                             <p style={{
                                                 width: '100%',
@@ -152,11 +197,11 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
                                             <p style={{
                                                 display: 'block',
                                                 textAlign: 'end'
-                                            }} className="w-full font-medium">{totalDelayToast}</p>
+                                            }} className='w-full font-medium'>{totalDelayToast}</p>
                                         </div>
                                     </div>
 
-                                    <div className="flex justify-between py-[.1rem] text-green-600 monotext">
+                                    <div className='flex justify-between py-[.1rem] text-green-600 monotext'>
                                         <div className='w-[50%]'>
                                             <p style={{
                                                 display: 'block',
@@ -167,13 +212,13 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
                                             <p style={{
                                                 display: 'block',
                                                 textAlign: 'end'
-                                            }} className="font-medium">{totalDelayDelivety}</p>
+                                            }} className='font-medium'>{totalDelayDelivety}</p>
                                         </div>
                                     </div>
                                 </div>
 
 
-                                <div className="w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]">
+                                <div className='w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]'>
                                     <div className='flex flex-row gap-[.5rem] text-lg font-semibold text-gray-800 border-b border-gray-200 pb-[.5rem] mb-[.5rem]'>
                                         <img className='w-[25px] h-[25px]' src={icoReloj} alt='ico-grafuc' />
                                         <h2>Tiempos Promedio</h2>
@@ -187,33 +232,33 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
 
 
                             <div className='w-full h-[50%] flex direction-row justify-center justify-around'>
-                                <div className="w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]">
+                                <div className='w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]'>
                                     <div className='flex flex-row gap-[.5rem] text-lg font-semibold text-gray-800 border-b border-gray-200 pb-[.5rem] mb-[.5rem]'>
                                         <img className='w-[25px] h-[25px]' src={icoProcess} alt='ico-grafuc' />
                                         <h2>Procesos por Categoría</h2>
                                     </div>
-                                    <div className="flex flex-col gap-[.2rem] justify-between py-[.1rem] border-b border-gray-100 monotext" key=''>
-
+                                    <div className='flex flex-col gap-[.2rem] justify-between py-[.1rem] border-b border-gray-100 monotext' key=''>
+                                        {process(true)}
                                     </div>
                                 </div>
 
 
-                                <div className="w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]">
+                                <div className='w-[48%] bg-white rounded-lg shadow-sm border border-gray p-[.5rem]'>
                                     <div className='flex flex-row gap-[.5rem] text-lg font-semibold text-gray-800 border-b border-gray-200 pb-[.5rem] mb-[.5rem]'>
                                         <img className='w-[25px] h-[25px]' src={icoAlert} alt='ico-grafuc' />
                                         <h2>Demoras de preparación por categorias</h2>
                                     </div>
-                                    <div className="bg-green-50 rounded-md p-3 mb-4">
+                                    <div className='bg-green-50 rounded-md p-3 mb-4'>
                                         {
                                             dishItem.map(dish => {
                                                 const delay = dataState.delayToastPost.delay.filter(delay => delay.nameDish === dish.nameDishe);
                                                 return (
-                                                    <div className="flex justify-between py-[.1rem] border-b border-gray-100 monotext" key={`metric${dish.nameDishe}`}>
+                                                    <div className='flex justify-between py-[.1rem] border-b border-gray-100 monotext' key={`metric${dish.nameDishe}`}>
                                                         <div>
-                                                            <p className="text-gray-600">{dish.nameDishe}:</p>
+                                                            <p className='text-gray-600'>{dish.nameDishe}:</p>
                                                         </div>
                                                         <div>
-                                                            <p className="font-medium text-green-600">{delay.length}</p>
+                                                            <p className='font-medium text-green-600'>{delay.length}</p>
                                                         </div>
                                                     </div>
                                                 )
@@ -221,12 +266,12 @@ export default memo(function Summary({ dataState, dishItem, styles, config, put_
 
                                         }
                                     </div>
-                                    <div className="flex justify-between py-[.1rem] text-green ">
+                                    <div className='flex justify-between py-[.1rem] text-green'>
                                         <div>
-                                            <span className="font-medium">Total superaciones</span>
+                                            <span className='font-medium'>Total superaciones</span>
                                         </div>
                                         <div>
-                                            <span className="font-medium monotext"></span>
+                                            <span className='font-medium monotext'>{totalDelayToast}</span>
                                         </div>
                                     </div>
                                 </div>
